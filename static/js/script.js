320d55b01c37bd265118656edabbbca7dbe7b2a4
winter <winterw201501@gmail.com>
2023-01-11 10:11:02 +0000

    modify some tips

--- static/js/script.js
+++ static/js/script.js
@@ -2306,7 +2306,7 @@ if (IS_HEADER) {
   init()
 } else {
   // Allow status to render, then preload assets and start app.
-  setLoadingStatus('献给你爱的TA ❤')
+  // setLoadingStatus('献给你爱的TA ❤')
   setTimeout(() => {
     soundManager.preload().then(init, (reason) => {
       // Codepen preview doesn't like to load the audio, so just init to fix the preview for now.
